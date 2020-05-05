5a232ba2bb67218e981c67b117c54fad80ef4cef
Marco Yuquilima <myuquilima@engicoders.com>
2020-05-05 14:24:39 -0500

    feat: Add methods to filter sums by username

--- sum-service/src/main/java/com/yuqmettal/sum/sumservice/repository/SumRepository.java
+++ sum-service/src/main/java/com/yuqmettal/sum/sumservice/repository/SumRepository.java
@@ -1,5 +1,8 @@
 package com.yuqmettal.sum.sumservice.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.yuqmettal.sum.sumservice.model.Sum;
 
 import org.springframework.data.repository.CrudRepository;
@@ -7,5 +10,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SumRepository extends CrudRepository<Sum, Long> {
-
+    public List<Sum> findByUserName(String username);
+    public Optional<Sum> findByIdAndUserName(Long id, String username);
 }
