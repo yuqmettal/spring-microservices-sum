f6b53a534ce53cd9e99f0e0fe10c5b6d39eae271
Marco Yuquilima <myuquilima@engicoders.com>
2020-05-05 14:54:19 -0500

    refactor: Change query to get users

--- users-service/src/main/java/com/yuqmettal/sum/usersservice/repository/UserRepository.java
+++ users-service/src/main/java/com/yuqmettal/sum/usersservice/repository/UserRepository.java
@@ -5,5 +5,5 @@ import com.yuqmettal.sum.usersservice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    public User findByEmail(String username);
+    public User findByUsername(String username);
 }
