fcec65898dd97d85d845f9f0488160d90ea03c16
Marco Yuquilima <myuquilima@engicoders.com>
2020-05-05 15:42:23 -0500

    feat: Add roles to created users

--- users-service/src/main/java/com/yuqmettal/sum/usersservice/service/CustomUserService.java
+++ users-service/src/main/java/com/yuqmettal/sum/usersservice/service/CustomUserService.java
@@ -1,9 +1,14 @@
 package com.yuqmettal.sum.usersservice.service;
 
+import java.util.List;
+
+import com.yuqmettal.sum.usersservice.entity.Role;
 import com.yuqmettal.sum.usersservice.entity.User;
+import com.yuqmettal.sum.usersservice.repository.RoleRepository;
 import com.yuqmettal.sum.usersservice.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +19,12 @@ public class CustomUserService implements UserService {
     private UserRepository userRepository;
 
     @Autowired
-    private BCryptPasswordEncoder bcryptEncoder;
+    private RoleRepository roleRepository;
+
+    @Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
     @Override
     public User signUp(User user) {
@@ -22,7 +32,9 @@ public class CustomUserService implements UserService {
         if (dbUser != null) {
             throw new RuntimeException("User already exist.");
         }
-        user.setPassword(bcryptEncoder.encode(user.getPassword()));
+        List<Role> userRoles = roleRepository.findByName("ROLE_USER");
+        user.setRoles(userRoles);
+        user.setPassword(passwordEncoder().encode(user.getPassword()));
         return userRepository.save(user);
     }
     
