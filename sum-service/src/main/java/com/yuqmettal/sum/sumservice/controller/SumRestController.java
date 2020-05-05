8f218e861d686289dbd308d8688ea34cb73410a2
Marco Yuquilima <myuquilima@engicoders.com>
2020-05-05 17:07:27 -0500

    refactor: remove unused libraries

--- sum-service/src/main/java/com/yuqmettal/sum/sumservice/controller/SumRestController.java
+++ sum-service/src/main/java/com/yuqmettal/sum/sumservice/controller/SumRestController.java
@@ -10,8 +10,6 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import com.yuqmettal.sum.sumservice.model.Sum;
 import com.yuqmettal.sum.sumservice.repository.SumRepository;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
