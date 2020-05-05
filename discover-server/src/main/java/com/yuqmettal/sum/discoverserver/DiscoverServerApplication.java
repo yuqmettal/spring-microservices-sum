46b694958fffd815502027780da7ee9345f37498
Marco Yuquilima <myuquilima@engicoders.com>
2020-05-05 07:51:43 -0500

    feat: Configure discover server

---
+++ discover-server/src/main/java/com/yuqmettal/sum/discoverserver/DiscoverServerApplication.java
@@ -0,0 +1,15 @@
+package com.yuqmettal.sum.discoverserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoverServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoverServerApplication.class, args);
+	}
+
+}
