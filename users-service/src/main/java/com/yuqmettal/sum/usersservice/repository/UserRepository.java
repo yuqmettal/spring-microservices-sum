87eca4cea0642bb7326c32e3608365fb0dbee28f
Marco Yuquilima <myuquilima@engicoders.com>
2020-05-05 14:52:14 -0500

    feat: Add user repository

--- users-service/src/main/java/com/yuqmettal/sum/usersservice/repository/UserRepository.java
+++ users-service/src/main/java/com/yuqmettal/sum/usersservice/repository/UserRepository.java
@@ -2,15 +2,8 @@ package com.yuqmettal.sum.usersservice.repository;
 
 import com.yuqmettal.sum.usersservice.entity.User;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@RepositoryRestResource(path = "users")
-public interface UserRepository extends PagingAndSortingRepository<User, Long> {
-	
-	@RestResource(path = "find-by-username")
-	public User findByUsername(@Param("username") String username);
-
+public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByEmail(String username);
 }
