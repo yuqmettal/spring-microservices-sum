f0b6641b280c5bd50f3a3ef544c03861f2235c4b
Marco Yuquilima <myuquilima@engicoders.com>
2020-05-05 21:27:35 -0500

    feat: Add config client

--- sum-service/src/main/java/com/yuqmettal/sum/sumservice/SumServiceApplication.java
+++ sum-service/src/main/java/com/yuqmettal/sum/sumservice/SumServiceApplication.java
@@ -3,8 +3,10 @@ package com.yuqmettal.sum.sumservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @EnableEurekaClient
+@RefreshScope
 @SpringBootApplication
 public class SumServiceApplication {
 
