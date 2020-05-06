dc15f54a6b5d37324ad767c590090259cdb34791
Marco Yuquilima <myuquilima@engicoders.com>
2020-05-05 21:31:05 -0500

    feat: Add config client to user service

--- users-service/src/main/java/com/yuqmettal/sum/usersservice/UsersServiceApplication.java
+++ users-service/src/main/java/com/yuqmettal/sum/usersservice/UsersServiceApplication.java
@@ -2,8 +2,10 @@ package com.yuqmettal.sum.usersservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class UsersServiceApplication {
 
 	public static void main(String[] args) {
